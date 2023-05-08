8d3355801d50ad3911f07288da0e60350c69e2a6
moxg <moxg22@outlook.com>
2023-05-08 10:10:21 -0400

    ads

---
+++ src/main/java/net/moxg/souls/Souls.java
@@ -0,0 +1,15 @@
+package net.moxg.souls;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Souls implements ModInitializer {
+	public static final String MOD_ID = "souls";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/moxg/souls/SoulsClient.java
@@ -0,0 +1,10 @@
+package net.moxg.souls;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SoulsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
